f9019903a3d50213ef0c7acabcb84678ed36bb66
vacatixn <danytokmakov@gmail.com>
2023-02-05 15:45:15 +0500

    Add qractivity layout

--- app/src/main/java/com/example/blooddonation/MainActivity.java
+++ app/src/main/java/com/example/blooddonation/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.blooddonation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.ExperimentalGetImage;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    @ExperimentalGetImage
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void  onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
