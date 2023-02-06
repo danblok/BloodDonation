6431ce4e672611a7bd95ef6226c8b553e6a61691
vacatixn <danytokmakov@gmail.com>
2023-02-07 00:49:30 +0500

    Change model

--- app/src/main/java/com/example/blooddonation/Application.java
+++ app/src/main/java/com/example/blooddonation/Application.java
@@ -2,17 +2,18 @@ package com.example.blooddonation;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class Application {
+public class Application implements Serializable {
 
-    private int id;
+    private String id;
     @JsonFormat(pattern="yyyy-MM-dd")
     private Date due;
     private User user;
 
 
-    public Application(int id, Date due) {
+    public Application(String id, Date due) {
         this.id = id;
         this.due = due;
     }
--- app/src/main/java/com/example/blooddonation/User.java
+++ app/src/main/java/com/example/blooddonation/User.java
@@ -2,48 +2,154 @@ package com.example.blooddonation;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
-public class User {
+public class User implements Serializable {
 
-    private int id;
+    private String id;
     private String firstName;
     private String lastName;
     private String middleName;
     private String email;
+    private String password;
     @JsonFormat(pattern = "yyyy-MM-dd")
     private Date dateOfBirth;
+    private String sex;
+    private String city;
+    private String bloodType;
+    private String seriesAndNumber;
+    private List<String> bloodComponents;
+    private String role;
 
-    public User(int id, String firstName, String lastName, String middleName, String email, Date dateOfBirth) {
-        this.id = id;
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String middleName, String email, String password) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.middleName = middleName;
         this.email = email;
-        this.dateOfBirth = dateOfBirth;
+        this.password = password;
     }
 
-    public int getId() {
+    public User(String firstName, String lastName, String middleName, String email, String password, Date dateOfBirth, String sex, String city, String bloodType, String seriesAndNumber, List<String> bloodComponents, String role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.middleName = middleName;
+        this.email = email;
+        this.password = password;
+        this.dateOfBirth = dateOfBirth;
+        this.sex = sex;
+        this.city = city;
+        this.bloodType = bloodType;
+        this.seriesAndNumber = seriesAndNumber;
+        this.bloodComponents = bloodComponents;
+        this.role = role;
+    }
+
+    public String getId() {
         return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
     public String getLastName() {
         return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getMiddleName() {
         return middleName;
     }
 
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public Date getDateOfBirth() {
         return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getBloodType() {
+        return bloodType;
+    }
+
+    public void setBloodType(String bloodType) {
+        this.bloodType = bloodType;
+    }
+
+    public String getSeriesAndNumber() {
+        return seriesAndNumber;
+    }
+
+    public void setSeriesAndNumber(String seriesAndNumber) {
+        this.seriesAndNumber = seriesAndNumber;
+    }
+
+    public List<String> getBloodComponents() {
+        return bloodComponents;
+    }
+
+    public void setBloodComponents(List<String> bloodComponents) {
+        this.bloodComponents = bloodComponents;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 }
